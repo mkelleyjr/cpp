5ccb24062d187942c8d07a2769814da6935f7cf4
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-03 21:29:02 -0400

    Basic user input example

---
+++ BasicInput.cpp
@@ -0,0 +1,27 @@
+/* Michael L. Kelley Jr.
+// November 3, 2021
+// BasicInput.cpp
+// Basic user input
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Variables to hold a string and an integer 
+    string UserName; 
+    int UserNumber;
+
+    // Ask the user for their name and process it 
+    cout << "Enter your name:" << endl;
+    cin >> UserName;
+    cout << "Hello, there " << UserName << endl;
+
+    // Ask the user for a number and process it 
+    cout << "Enter a number:" << endl;
+    cin >> UserNumber;
+    cout << "Oh, you picked: " << endl;
+
+    return 0;
+}

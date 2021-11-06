bc01825fbe9754d043b9003f95843dff63a12bd2
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-05 21:02:31 -0400

    Hello world example

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+/* Michael L. Kelley Jr.
+// November 5, 20201
+// HelloWorld.cpp
+
+// Class example in C++
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World from C++" << endl;
+
+    return 0; 
+}

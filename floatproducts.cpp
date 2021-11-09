418205aec2a7cfba3e062483fcdda03d5a2e2222
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-09 00:49:29 -0500

    product of 2 floats

---
+++ floatproducts.cpp
@@ -0,0 +1,28 @@
+/* Michael L. Kelley Jr.
+// November 9, 2021
+// floatproducts.cpp
+
+// Get the product of two floats
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a;
+    float b;
+
+    cout << "Enter the 1st floating number:" << endl;
+    cin >> a;
+
+    cout << "Enter the 2nd floating number" << endl;
+    cin >> b;
+
+    c = a*b;
+
+    cout <<"The prodcut of your two numbers is: " << c << endl;
+
+    return 0;
+}
+

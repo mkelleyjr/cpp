fc1aab1d50ee75c516a8d1d9ac50a2b661d64ba3
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-10 00:13:37 -0500

    modulus example

---
+++ modulus.cpp
@@ -0,0 +1,20 @@
+/* Michael L. Kelley Jr.
+// November 10, 2021
+// modulus.cpp
+
+// modulus operator example
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Modulus operator example of 7 % 3" << endl;
+    int mod = 7 % 3;
+
+    // % operator returns the remainder 
+    cout << mod;
+
+    return 0; 
+}

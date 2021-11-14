1766ba0d033bb9b42dcb12be9ebc563f4e54ea71
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-14 00:27:08 -0500

    for loop

---
+++ .vscode/forloop.cpp
@@ -0,0 +1,21 @@
+/* Michael L. Kelley Jr.
+// November 14, 2021
+// forloop.cpp
+
+// simple for loop example - quickly print an int 100 times 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int num = 1; 
+   
+    // for loop code 
+    for(int i = 0; i <= 100; i++) {
+        cout << num; 
+    }
+
+    return 0; 
+}

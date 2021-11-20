9e0b9e1bf82e775970cdc01393628bf219ec77a0
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-20 01:39:33 -0500

    counting a string

---
+++ .vscode/countstring.cpp
@@ -0,0 +1,19 @@
+/* Michael L. Kelley Jr.
+// November 20, 2021
+// countstring.cpp
+
+//  read chars in a string 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    string str = "numberofchars";
+    cout << str.size(); // read & print string size 
+    
+    return 0; 
+}
+

01f18032f0b80c4c769400a97ee266764dee2984
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-21 01:40:35 -0500

    cin example >>

---
+++ cin.cpp
@@ -0,0 +1,25 @@
+/* Michael L. Kelley Jr.
+// November 21, 2021
+// cin.cpp 
+
+// cin example with multiple >> >> >> 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int a;
+int b;
+int c;
+
+cout <<"Enter 3 numbers:" << endl;
+cin >> a >> b >> c; 
+
+cout << a << b << c; 
+
+return 0; 
+
+}

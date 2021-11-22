45818ebc740d9a6511ea03ee577977ffcdc0467c
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-22 00:13:10 -0500

    average of 4 numbers program

---
+++ .vscode/numaverage.cpp
@@ -0,0 +1,39 @@
+/* Michael L. Kelley Jr.
+// November 22, 2021
+// numaverage.cpp
+
+// get 4 numbers and return the total and average of them 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d; 
+    int total; 
+    float average; 
+
+    cout << "Enter 1st number:" << endl;
+    cin >> a;
+    cout << "Enter 2nd number:" << endl;
+    cin >> b;
+    cout << "Enter 3rd number:" << endl;
+    cin >> c;
+    cout << "Enter 4th number:" << endl;
+    cin >> d;
+    
+    // get the total 
+    total = a+b+c+d;
+    cout << "The total of the numbers is: " << total << endl;
+
+    // get the average
+    average = total / 4; 
+    cout << "The average of those numbers is: " << average << endl;
+
+    return 0; 
+
+}

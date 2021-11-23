0bfb538f3a84b082f8a1e80b066d722dde75da1d
mkelleyjr <michael.kelley.jr@gmail.com>
2021-11-23 00:24:24 -0500

    game over example

---
+++ gameover.cpp
@@ -0,0 +1,17 @@
+/* Michael L. Kelley Jr.
+// November 23, 2021
+// gameover.cpp
+
+// beginnign c++ game programming program #1
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // game over message 
+    cout << "Game Over!" << endl;
+
+    return 0; 
+}

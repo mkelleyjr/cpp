7281e2d4ba1826925b1e4b14ff5bb7b9dab0a299
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2022-01-01 22:04:22 -0500

    Starting off year of code 2022

---
+++ Hello2022.cpp
@@ -0,0 +1,14 @@
+/* Michael L. Kelley Jr.
+// January 1, 2022
+// Hello.cpp
+
+// Year of Code in 2022
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello from 2022!" << endl;
+}

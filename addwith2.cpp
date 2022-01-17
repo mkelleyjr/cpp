1115c857014e4d6202294736723b4ae5f15b3eef
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2022-01-17 13:00:25 -0500

    added more comments

--- addwith2.cpp
+++ addwith2.cpp
@@ -14,15 +14,19 @@ int main()
     int b;
     int c;
 
+    // Get 1st number 
     cout << "Enter 1st number:" << endl;
     cin >> a;
 
+    // Get 2nd number 
     cout << "Enter 2nd number:" << endl;
     cin >> b;
 
+    // Display the user's numbers 
     cout << "Input 1st number: " << a << endl;
     cout << "Input 2nd number: " << b << endl;
 
+    // Calculate 
     c = a + b; 
     cout << "The sum of the numbers is: " << c << endl;
 

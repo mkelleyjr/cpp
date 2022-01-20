df5a41f501bd5b8b107fbebfdcf95bf37b795b4d
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2022-01-19 19:37:07 -0500

    added comments.

--- heronformula.cpp
+++ heronformula.cpp
@@ -10,12 +10,14 @@ using namespace std;
 
 int main()
 {
+    // Variables used 
     float a; 
     float b;
     float c;
     float area;
     float s; 
 
+    // Get the 3 lenghts of triangle from user 
     cout << "Enter the length of 1st side of triangle:" << endl;
     cin >> a;
     cout << "Enter the length of 2nd side of the triangle:" << endl;
@@ -23,9 +25,11 @@ int main()
     cout << "Enter the length of 3rd side of hte triangle:" << endl;
     cin >> c;
 
+    // Calculate the area using formulas 
     s = (a + b + c)/2;
     area = sqrt(s*(s-a)*(s-b)*(s-c));
 
+    // Display to the user the area 
     cout << "The area of the triangle is : " << area << endl;
     cout << endl;
     return 0; 

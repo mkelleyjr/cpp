972fbdd368adac9285d25f0a2432e304cf143175
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2022-01-25 18:05:25 -0500

    concatenation example

---
+++ Untitled-1.cpp
@@ -0,0 +1,21 @@
+/* Michael L. Kelley Jr.
+// January 25, 2022
+// Concatenate.cpp
+// Put 2 strings together
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    string word1 = "Good";
+    string word2("Bye"); 
+    string phrase; 
+
+    phrase = word1 + word2; 
+    cout "The phrase is: " << phrase << endl; 
+
+    return 0; 
+}

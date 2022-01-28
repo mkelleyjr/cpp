e0fa43da94936c96a224ac9300cb8e3bf8774bbf
Michael L. Kelley Jr <michael.kelley.jr@gmail.com>
2022-01-27 21:08:50 -0500

    comments

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,7 +1,7 @@
 /* Michael L. Kelley Jr.
 // January 25, 2022
 // Concatenate.cpp
-// Put 2 strings together
+// Put 2 strings together and display them 
 */
 
 #include<iostream>
@@ -14,7 +14,7 @@ int main()
     string word2("Bye"); 
     string phrase; 
 
-    // put the strings together 
+    // put the strings together and dispaly the phrase 
     phrase = word1 + word2; 
     cout "The phrase is: " << phrase << endl; 
 
